a758ce03f74daac70c83af4c632a0a85a5eab95f
YamamotoU518 <hw37hnmr@gmail.com>
2024-06-05 12:46:49 +0900

    [Add] 公文式課題　レベル3

---
+++ Test_GameSeisaku/Assets/Scripts/CubeGenerate.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGenerate : MonoBehaviour
+{
+    [SerializeField] GameObject _obj;
+    [SerializeField] GameObject _startObj;
+    [SerializeField] GameObject _goalObj;
+    float _start;
+    float _goal;
+
+    void Start()
+    {
+        _start = _startObj.transform.position.z - 5;
+        _goal = _goalObj.transform.position.z;
+        for (float i = _start; i > _goal; i -= 5)
+        {
+            Instantiate(_obj, new Vector3(0, 1, i), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
